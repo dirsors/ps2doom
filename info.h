94ff3c59f0c0af2b47f386d67c31f411e86b4f28
7dog123 <futuretrunks745@gmail.com>
2020-05-31 19:17:20 +0000

    fixed id-Software mess up with pain elementals. They gave the pain elementals a raise state while they leave no corps.

--- info.h
+++ info.h
@@ -1,4 +1,4 @@
-// Emacs style mode select   -*- C++ -*- 
+	// Emacs style mode select   -*- C++ -*- 
 //-----------------------------------------------------------------------------
 //
 // $Id:$
@@ -893,12 +893,6 @@ typedef enum
     S_PAIN_DIE4,
     S_PAIN_DIE5,
     S_PAIN_DIE6,
-    S_PAIN_RAISE1,
-    S_PAIN_RAISE2,
-    S_PAIN_RAISE3,
-    S_PAIN_RAISE4,
-    S_PAIN_RAISE5,
-    S_PAIN_RAISE6,
     S_SSWV_STND,
     S_SSWV_STND2,
     S_SSWV_RUN1,
